df425b18a4432dbe383c4633791bbb54fa52b15c
Orion Edwards <orion.edwards@gmail.com>
2015-11-01 07:17:25 +1300

    Initial Commit

---
+++ SerialQueueCPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SerialQueueCPP
+//
+//  Created by Orion Edwards on 1/11/15.
+//  Copyright © 2015 Orion Edwards. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
